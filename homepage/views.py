3b2c144d21af2378598c3b50fa8b39b2b619265f
SauravStha1 <sauravshrestha186@gmail.com>
2026-02-19 10:41:30 +0545

    feat: search feature added

--- homepage/views.py
+++ homepage/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
+from django.db.models import Q
 from Job_Post.models import Job
 
-
 def home(request):
-    jobs = Job.objects.all().order_by('-created_at')
+    query = request.GET.get('q')
+    job_type = request.GET.get('job_type')
+
+    jobs = Job.objects.all()
+
+    if query:
+        jobs = jobs.filter(
+            Q(title__icontains=query) | Q(tags__icontains=query)
+        )
+
+    if job_type and job_type != "All":
+        jobs = jobs.filter(job_type=job_type)
+
+    jobs = jobs.order_by('-created_at')
+
     return render(request, 'homepage/home.html', {
-        'jobs': jobs
+        'jobs': jobs,
+        'query': query,
+        'selected_job_type': job_type
     })
