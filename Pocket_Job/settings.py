3213e0a0d40a745a55d3f9d2f1ddc0a5df4413ea
SauravStha1 <sauravshrestha186@gmail.com>
2026-04-14 20:25:15 +0545

    feat: database changed to postgres

--- Pocket_Job/settings.py
+++ Pocket_Job/settings.py
@@ -98,8 +98,12 @@ WSGI_APPLICATION = 'Pocket_Job.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Pocket_Job',
+        'USER': 'postgres',
+        'PASSWORD': '5756473',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
